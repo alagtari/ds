4ea731639d19f33f6870cb0552e6d31145d27421
alagtari <alagtari@yahoo.fr>
2023-11-09 10:30:33 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+const MONGODB_URI = "mongodb://127.0.0.1:27017/Books";
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", (error) => {
+  console.error("MongoDB connection error:", error);
+});
+
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Acces-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Acces-Control-Allow-Header",
+    "Origin, X-Requested-With, Content, Accept, Content-Type ,Authorization"
+  );
+  res.setHeader(
+    "Acces-Control-Allow-Methods",
+    "GET, POST, PUT , DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+module.exports =app
