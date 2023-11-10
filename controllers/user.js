2706e82e013398bdd1557fced9fc6e70662b3bae
alagtari <alagtari@yahoo.fr>
2023-11-10 10:54:57 +0100

    get author info

--- controllers/user.js
+++ controllers/user.js
@@ -8,25 +8,42 @@ const validation = (req, res, next) => {
         return res.status(401).json({ message: "Author not found !" });
       }
       bcrypt.hash(user.phone, 10).then((hash) => {
-        user.password = hash
-        user.status = "V"
+        user.password = hash;
+        user.status = "V";
         user
-        .save()
-        .then((response) => {
-          const newUser = response.toObject();
-          delete newUser.password;
-          res.status(200).json({
-            payload: newUser,
-            message: "Author verified!",
-          });
-        })
-        .catch((error) => res.status(400).json({ error: error.message }));
+          .save()
+          .then((response) => {
+            const newUser = response.toObject();
+            delete newUser.password;
+            res.status(200).json({
+              payload: newUser,
+              message: "Author verified!",
+            });
+          })
+          .catch((error) => res.status(400).json({ error: error.message }));
       });
     })
     .catch((error) => res.status(500).json({ error: error }));
 };
 
+const info = (req, res, next) => {
+  User.findOne({ _id: req.auth.userId })
+    .populate("publications")
+    .exec()
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({ message: "Author not found !" });
+      } else {
+        res.status(200).json({
+          payload: user,
+          message: "Success",
+        });
+      }
+    })
+    .catch((error) => res.status(400).json({ error: error.message }));
+};
 
 module.exports = {
-    validation
-  };
+  validation,
+  info,
+};
--- routes/auth.js
+++ routes/auth.js
@@ -8,5 +8,7 @@ router.post("/register",authController.signup)
 router.post("/login",authController.login)
 router.post("/add-admin",authController.addAdmin)
 router.post("/verify",authMiddleware.loggedMiddleware,authMiddleware.isAdmin,userController.validation)
+router.get("/posts/author/details",authMiddleware.loggedMiddleware,authMiddleware.isAuthor,userController.info)
+
 
 module.exports =router
